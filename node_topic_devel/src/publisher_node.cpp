1336ab9fd21c464bc3ed980f621a9c7516c1c02e
FabianDumitrascu <fabiandumitrascu@hotmail.com>
2024-05-13 11:14:04 +0200

    feat: publish/subscriber   package

--- node_topic_devel/src/publisher_node.cpp
+++ node_topic_devel/src/publisher_node.cpp
@@ -1,18 +1,20 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "node_topic_devel/person_data.h"
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "Publisher");
     ros::NodeHandle nh;
 
-    ros::Publisher topic_pub = nh.advertise<std_msgs::String>("tutorial", 1000); 
+    ros::Publisher topic_pub = nh.advertise<node_topic_devel::person_data>("tutorial", 1000); 
     ros::Rate loop_rate(1);
 
     while(ros::ok()) {
-        std_msgs::String msg;
-        msg.data = "Hello World!";
+        node_topic_devel::person_data person_data;
+        person_data.name = "John";
+        person_data.age = 25;
+        person_data.color = "blue";
 
-        topic_pub.publish(msg);
+        topic_pub.publish(person_data);
         ros::spinOnce();
         loop_rate.sleep();
 
--- node_topic_devel/src/subscriber_node.cpp
+++ node_topic_devel/src/subscriber_node.cpp
@@ -1,10 +1,21 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "node_topic_devel/person_data.h"
+
+void callback(const node_topic_devel::person_data &person_data){
+    ROS_INFO("I heard name: [%s]", person_data.name.c_str());
+    ROS_INFO("I heard age: [%d]", person_data.age);
+    ROS_INFO("I heard color: [%s]", person_data.color.c_str());
+}
 
 int main(int argc, char **argv){
-    ros::Init(argc, argv, "Subscriber");
+    ros::init(argc, argv, "Subscriber");
     ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("tutorial", 1000, callback);
+    ros::spin();
+
+
     
 
-    return 0
+    return 0;
 }
